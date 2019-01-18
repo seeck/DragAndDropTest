3a3c08839dfeeb92bed2f6183865f7c1cb501260
DESKTOP-JR36UBV\Chaitanya Kochhar <chaitanya@smartypal.com>
2019-01-17 23:44:50 -0800

    Added SafeInvoke to safely handle null delegates

---
+++ Assets/Scripts/Utils/Utils.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public static class Utils {
+    public static void SafeInvoke<T1,T2>(this Action<T1,T2> action, T1 arg1, T2 arg2)
+    {
+        if(action!=null)
+        {
+            action(arg1,arg2);
+        }
+    }
+}
