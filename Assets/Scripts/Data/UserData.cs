3ba22ef32b094ac73322752528081c01d5cc63a6
DESKTOP-JR36UBV\Chaitanya Kochhar <chaitanya@smartypal.com>
2019-01-17 23:48:56 -0800

    Created UserData to house the initial song and contact data (this information is rarely static, it comes down in some format from a server)

---
+++ Assets/Scripts/Data/UserData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserData
+{
+    public List<ListItemData> Songs { get; private set; }
+    public List<ListItemData> Contacts { get; private set; }
+
+    public UserData()
+    {
+        Songs = new List<ListItemData>();
+        Contacts = new List<ListItemData>();
+    }
+
+    public void ParseSongs(string[] songs)
+    {
+        if (songs == null)
+        {
+            return;
+        }
+        foreach (string song in songs)
+        {
+            ListItemData data = new ListItemData(song, ItemDataType.Music);
+            Songs.Add(data);
+        }
+    }
+
+    public void ParseContacts(string[] contacts)
+    {
+        if (contacts == null)
+        {
+            return;
+        }
+        foreach (string contact in contacts)
+        {
+            ListItemData data = new ListItemData(contact, ItemDataType.Contacts);
+            Contacts.Add(data);
+        }
+    }
+}
