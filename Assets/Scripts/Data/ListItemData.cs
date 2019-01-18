59d6585e1dfe78ff02f9f2dae5730ea6479bc4e6
DESKTOP-JR36UBV\Chaitanya Kochhar <chaitanya@smartypal.com>
2019-01-17 23:47:59 -0800

    Created derived ListItem class to house the ListItemData (label and data type) ListItemData class contains the data associated with a given ListItem UI item

---
+++ Assets/Scripts/Data/ListItemData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListItemData {
+    public string Name { get; private set; }
+    public ItemDataType DataType { get; private set; }
+
+    public ListItemData(string name, ItemDataType dataType)
+    {
+        Name = name;
+        DataType = dataType;
+    }
+}
+
+public enum ItemDataType {
+    Music,
+    Contacts
+}
---
+++ Assets/Scripts/UI/ListItem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+/// <summary>
+/// Uses ListItemData to populate its UI.
+/// Populated via the Manager (controller)
+/// Derives from Draggable so that the dragging logic is kept separate from the data.
+/// </summary>
+public class ListItem : Draggable
+{
+    [SerializeField]
+    Text displayName;
+
+    ListItemData data;
+    public ListItemData Data
+    {
+        get
+        {
+            return data;
+        }
+        set
+        {
+            data = value;
+            Refresh();
+        }
+    }
+
+    public bool IsShortcut { get; set; }
+
+    public void Refresh()
+    {
+        if(data == null)
+        {
+            displayName.text = "";
+            return;
+        }
+        displayName.text = data.Name;
+    }
+}
